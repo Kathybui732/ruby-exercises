99ad2c0c65367c221486ea69e8bf3ada2a7b14aa
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-04 20:24:00 -0600

    Run test 19

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -190,10 +190,10 @@ class StringTest < Minitest::Test
   end
 
   def test_19
-    skip
+    # skip
     greeting = "Hello World!"
     # In place of the line below, call a method to check if the string includes 'lol'
-    actual = greeting._____
+    actual = greeting.include?("lol")
     expected = false
 
     assert_equal expected, actual
