b1f4acd7cb7257afe78b8c2da785e9950a2bcb67
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-04 20:45:14 -0600

    Run test 21 with only one method

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -233,7 +233,7 @@ class StringTest < Minitest::Test
     # skip
     phrase = "   \n  \tto the    moon\n\n\t    "
     # In place of the line below, call a method to acheive the expected outcome
-    actual = phrase.strip.chomp
+    actual = phrase.strip
     expected = "to the    moon"
 
     assert_equal expected, actual
@@ -243,7 +243,7 @@ class StringTest < Minitest::Test
     skip
     phrase = "   \n  \tto the    moon\n\n\t    "
     # In place of the line below, call a method to acheive the expected outcome
-    actual = phrase
+    actual = phrase.strip
     expected = "   \n  \tto the    moon"
 
     assert_equal expected, actual
