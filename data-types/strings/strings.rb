77b37357819e26702bc86e706f091e3e78a489a1
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-04 20:11:03 -0600

    Run test 10

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -100,10 +100,10 @@ class StringTest < Minitest::Test
   end
 
   def test_10
-    skip
+    # skip
     greeting = "Hello!!\n\n"
     # In place of the line below, call a method to achieve the expected output.
-    actual = greeting._____
+    actual = greeting.chomp
     expected = "Hello!!\n"
 
     assert_equal expected, actual
