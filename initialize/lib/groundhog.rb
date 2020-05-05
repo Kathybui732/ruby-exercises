17c173d2678e1c177521a19e7ffd96b5ffe93b08
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-05 10:57:24 -0600

    Update groundhog to only use attr_reader

--- initialize/lib/groundhog.rb
+++ initialize/lib/groundhog.rb
@@ -1,7 +1,20 @@
+# class Groundhog
+#   attr_accessor :name
+# # have to use accessor if you want to be able to write something?
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+#
+
 class Groundhog
-  attr_accessor :name
-# have to use accessor if you want to be able to write something
+  attr_reader :name
+
   def initialize(name)
     @name = name
+  end
+
+  def name= (give_name)
+    @name = give_name
   end
 end
