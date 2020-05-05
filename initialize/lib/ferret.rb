8717594b13fbddb635f0cfb10e11b1667ccb550d
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-04 22:23:46 -0600

    Create class for ferret

---
+++ initialize/lib/ferret.rb
@@ -0,0 +1,11 @@
+class Ferret
+  attr_reader :name
+
+  def initialize
+    @name = "a ferret has no name"
+  end
+
+  def give_name(new_name)
+    @name = "a ferret's name is #{new_name}"
+  end
+end
