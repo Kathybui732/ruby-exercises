45c86dd3ea0ccd8c1ac4c3b14ed1f3a0c91db610
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-07 08:46:30 -0600

    Create roll call test

---
+++ command-query/exercises/roll_call.rb
@@ -0,0 +1,16 @@
+class RollCall
+
+  def initialize
+    @names = []
+  end
+
+  def <<(name)
+    @names << name
+  end
+
+  def longest_name
+    @names.max_by do |name|
+      name.length
+    end
+  end
+end
